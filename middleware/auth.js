81c0fb1c462d7dde41934dc7dbc34259701e9d1e
Gandy <1546329@senati.pe>
2025-08-22 13:25:29 -0500

    nuevos

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+  import jwt from 'jsonwebtoken';
+  import 'dotenv/config';
 
-const JWT_SECRET = process.env.JWT_SECRET;
+  const JWT_SECRET = process.env.JWT_SECRET;
 
-const authMiddleware = (req, res, next) => {
-  const token = req.header('x-auth-token');
+  const authMiddleware = (req, res, next) => {
+    const token = req.header('x-auth-token');
 
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, autorización denegada' });
-  }
+    if (!token) {
+      return res.status(401).json({ msg: 'No token, autorización denegada' });
+    }
 
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (e) {
-    res.status(401).json({ msg: 'El token no es válido' });
-  }
-};
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (e) {
+      res.status(401).json({ msg: 'El token no es válido' });
+    }
+  };
 
-export default authMiddleware;
+  export default authMiddleware;
